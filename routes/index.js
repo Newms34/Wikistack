6d051f0ac73165029d857247390c549484613b56
Newms34 <Newms3450@msn.com>
2015-01-29 15:48:49 -0500

    2nd  wiki commit

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/wikistack');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+ var models = require('../models/');
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/wikistack');
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var allDocs = db.wikistack.find();
+	var allDocs = models.Page.find();
+	console.log('-------LINE--------');
 	console.log(allDocs);
 
   res.render('index', { title: 'Express'});
